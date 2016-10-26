e29c15e3de92749d123eccbffbd24a15596dc1d1
carzy <carzy@benchmarkchina.com>
2016-10-26 11:25:15 +0800

    #130 init

--- src/main/java/com/bq/i1/Application.java
+++ src/main/java/com/bq/i1/Application.java
@@ -2,24 +2,14 @@ package com.bq.i1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import javax.sql.DataSource;
 
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 
