66f42f09a92841aef09dfb5bab046d8ad8ad44fc
bruce <bruce@angle-tech.com.tw>
2017-01-25 11:40:38 +0800

    hengda/i1-server#313 新增杂项类型基础资料维护 : 增加雜項類型 ,調撥類型 service.js

---
+++ src/main/resources/static/app/src/js/service/miscellaneousType.js
@@ -0,0 +1,26 @@
+angular.module('IOne-Production').service('MiscellaneousTypeService', function ($http, $filter, Constant) {
+    var requestUrl = Constant.BACKEND_BASE + '/miscellaneousTypes/';
+
+    this.add = function (input) {
+        return $http.post(requestUrl, input);
+    };
+
+    this.modify = function (uuid, updateInput) {
+        return $http.patch(requestUrl + uuid, updateInput);
+    };
+
+    this.delete = function (uuid) {
+        return $http.delete(requestUrl + uuid);
+    };
+
+    this.getAll = function (sizePerPage, currentPage, queryConditions) {
+        var config = {
+            params: angular.merge({}, {
+                size: sizePerPage,
+                page: currentPage,
+                resUuid: RES_UUID_MAP.INV.MISCELLANEOUS_TYPE.RES_UUID
+            }, queryConditions)
+        };
+        return $http.get(requestUrl, config);
+    };
+});
