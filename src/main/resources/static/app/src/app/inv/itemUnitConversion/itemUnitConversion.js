5804b67827456fdb509a27c111161cbaf533b129
bruce <bruce@angle-tech.com.tw>
2017-02-13 11:09:33 +0800

    hengda/i1-server#322 新增商品单位换算基础资料维护 : 增加表單新增鈕

--- src/main/resources/static/app/src/app/inv/itemUnitConversion/itemUnitConversion.js
+++ src/main/resources/static/app/src/app/inv/itemUnitConversion/itemUnitConversion.js
@@ -72,6 +72,8 @@ angular.module('IOne-Production').controller('ItemUnitConversionController', fun
      * Add new item which will take the ui to the edit page.
      */
     $scope.preAddItemAction = function (source, domain, desc) {
+        source.item = angular.copy($scope.selectedItem);
+        source.itemUuid = $scope.selectedItem.uuid;
         $scope.changeViewStatus(Constant.UI_STATUS.EDIT_UI_STATUS);
         $scope.status = 'add';
         $scope.desc = desc;
@@ -88,6 +90,7 @@ angular.module('IOne-Production').controller('ItemUnitConversionController', fun
                 $scope.source = response.data;
                 $scope.showInfo('新增数据成功。');
                 $scope.refreshList();
+                queryByDetailByItem($scope.selectedItem);
                 $scope.changeViewStatus(Constant.UI_STATUS.VIEW_UI_STATUS);
             }, errorHandle);
         } else if ($scope.status == 'edit') {
