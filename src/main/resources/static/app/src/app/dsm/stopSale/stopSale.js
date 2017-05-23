ee74c874858f1fe980f9cb9763b58e22925746be
gujian <gujian@benchmarkchina.com>
2017-05-23 17:23:56 +0800

    hengda/i1-server#422 【EPS】电商O2O区域配送资料同步:前端

--- src/main/resources/static/app/src/app/dsm/stopSale/stopSale.js
+++ src/main/resources/static/app/src/app/dsm/stopSale/stopSale.js
@@ -25,7 +25,8 @@ angular.module('IOne-Production').controller('StopSaleController', function ($sc
         PSO_DELIVER_ORDER_EXT_DTL: {value: 'PSO_DELIVER_ORDER_EXT_DTL', name: '出货同步'},
         EPS_DELIVER_ORDER_EXT_DTL: {value: 'EPS_DELIVER_ORDER_EXT_DTL', name: '电商出货单同步'},
         OCM_CHAN_BRAND: {value: 'OCM_CHAN_BRAND', name: '渠道品牌关系同步'},
-        EPS_O2O_MAIL: {value: 'EPS_O2O_MAIL', name: '电商线下门店同步'}
+        EPS_O2O_MAIL: {value: 'EPS_O2O_MAIL', name: '电商线下门店同步'},
+        EPS_O2O_AREA: {value: 'EPS_O2O_AREA', name: '电商O2O配送经销商同步'}
     };
 
     $scope.selected = [];
@@ -208,6 +209,17 @@ angular.module('IOne-Production').controller('StopSaleController', function ($sc
                     var totalTipTopTcQybFileCount = addResponse(response.updateTipTopTcQybFile, response.insertTipTopTcQybFile);
                     var totalSysBaseMapFileCount = addResponse(response.updateSysBaseMapFileCount, response.insertSysBaseMapFileCount);
                     $scope.showInfo('ERP同步到 TIPTOP_TC_QYB_FILE，共 ' + totalTipTopTcQybFileCount + '笔数据同步成功!\n TIPTOP_TC_QYB_FILE 同步到 SYS_BASE_MAP_FILE，共 ' + totalSysBaseMapFileCount + '笔数据同步成功!');
+                    $scope.logining = false;
+                }).error(function (errResp) {
+                    $scope.logining = false;
+                    $scope.showError(errResp.message);
+                });
+            } else if ($scope.listFilterOption.syncType.name == $scope.TIPTOP_SYNC_TYPE.EPS_O2O_AREA.name) {
+                // 电商O2O配送经销商对应资料同步
+                IoneAdapterService.transferIoneAdapter("/tcQyaTask", param, $scope, function (response) {
+                    var totalTipTopTcQyaFileCount = addResponse(response.updateTipTopTcQyaFile, response.insertTipTopTcQyaFile);
+                    var totalOcmBaseChanRCount = addResponse(response.updateOcmBaseChanRCount, response.insertOcmBaseChanRCount);
+                    $scope.showInfo('ERP同步到 TIPTOP_TC_QYA_FILE，共 ' + totalTipTopTcQyaFileCount + '笔数据同步成功!\n TIPTOP_TC_QYA_FILE 同步到 OCM_BASE_CHAN_R，共 ' + totalOcmBaseChanRCount + '笔数据同步成功!');
                     $scope.logining = false;
                 }).error(function (errResp) {
                     $scope.logining = false;
