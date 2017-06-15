2a9bee95091d6fbf3ce7f1969e4f56b4d56c1beb
gujian <gujian@benchmarkchina.com>
2017-06-15 15:50:45 +0800

    hengda/i1-server#444 渠道相关作业修正：渠道商品信息维护 渠道编号显示

--- src/main/resources/static/app/src/app/ocm/itemInfo/itemInfo.js
+++ src/main/resources/static/app/src/app/ocm/itemInfo/itemInfo.js
@@ -63,6 +63,16 @@ angular.module('IOne-Production').controller('ChannelItemInfoController', functi
         totalElements: 100
     };
 
+    $scope.queryEnter = function (e) {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            $scope.pageOption.currentPage = 0;
+            $scope.pageOption.totalPage = 0;
+            $scope.pageOption.totalElements = 0;
+            $scope.queryMenuAction();
+        }
+    };
+
     $scope.editItem = function (channel) {
         $scope.selectedItem = channel;
         $scope.changeViewStatus(Constant.UI_STATUS.PRE_EDIT_UI_STATUS, 1);
